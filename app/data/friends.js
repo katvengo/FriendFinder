71ab1d45e5a3955caefab181d090a072763a62c2
Kat Vengo <katvengo@gmail.com>
2019-05-25 00:56:53 -0700

    almost there

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "David",
+    "photo": "https://i.ibb.co/ZVG18MF/IMG-2621.jpg",
     "scores": [
         1,
         1,
@@ -16,8 +16,8 @@ var friends = [
     ]
 },
 {
-  "name": "Elizabeth",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name": "Kat",
+    "photo": "https://i.ibb.co/q7tKcFg/12227054-10153786500941212-1548934289189668126-n.jpg",
     "scores": [
         5,
         5,
